a6465428beda3e6af441ef05d2ea2462a0766cd1
MrLad01 <ladayodeji@gmail.com>
2023-10-21 08:10:16 +0100

    update

--- frontend/src/components/commentBox.tsx
+++ frontend/src/components/commentBox.tsx
@@ -85,7 +85,7 @@ export default function CommentBox(): React.ReactElement {
                 reply &&  <div className = 'w-full h-fit bg-white p-5 rounded-lg flex gap-4 mt-2 justify-around'>
                     <img src = { avatar2 } alt = ""  className = 'w-12 h-12' />
                     <textarea name = "" id = "" className = 'w-full h-28' ></textarea>
-                    <button className = 'py-1 px-5 bg-slate-400 h-14 ' > Reply </button>
+                    <button className = 'py-1 px-8 bg-slate-400 h-11 rounded-lg ' > REPLY </button>
                 </div>
 
             }
