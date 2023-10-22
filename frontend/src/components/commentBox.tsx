5ed8f6e5ccbff9e727e073506d18e19d4f69195d
MrLad01 <ladayodeji@gmail.com>
2023-10-22 09:16:52 +0100

    update

--- frontend/src/components/commentBox.tsx
+++ frontend/src/components/commentBox.tsx
@@ -16,25 +16,50 @@ export default function CommentBox(): React.ReactElement {
     const [ count, setCount ] = useState<number>(1);
     const [ reply, setReply ] = useState<boolean>(false);
 
+    const handleUpVote = () => {
+        setDownVote(false)
+        if ( downVote ){
+            setDownVote( false )
+            setCount( count + 1 )
+        } else if (!upVote) {   
+            setUpVote( true )
+            setCount( count + 1 )
+        }
+        else {
+            setCount( count - 1 )
+        }
+    }
+
+    const handleDownVote = () => {
+        setUpVote(false)
+        if ( upVote ){
+            setUpVote( false )
+            setCount( count - 1 )
+        } else if (!downVote) {   
+            setDownVote( true )
+            setCount( count - 1 )
+        }
+        else {
+            setCount( count + 1 )
+        }
+    }
+
+
+
 
     return (
         <>
             <div className = 'w-full h-fit bg-white p-5 rounded-lg flex gap-6'>
-
-                <div className = 'flex flex-col justify-around gap-4 items-center p-3 bg-light-grayish-blue rounded-2xl bg-opacity-30 h-28' >        
+                <div className = 'flex flex-col justify-around gap-3 items-center p-3 bg-light-grayish-blue rounded-2xl bg-opacity-30 h-24'>        
                     <button 
-                            disabled = { upVote }
-                            onClick = { () => {
-                                                setUpVote( true )
-                                                setDownVote(false)
-                                                setCount( downVote ? count + 2 : count + 1 )
-                                        }
-                                      }
+                        disabled = { upVote }
+                        onClick = { handleUpVote }
                     >
                         <img 
                             src = { plusIcon } 
                             alt = "plus" 
                             style = {{ filter: !upVote ? 'brightness(0.7)' : 'brightness(1)', }}  
+                            className = 'p-1'
                         />
                         <h2>  </h2>
                     </button>
@@ -43,34 +68,29 @@ export default function CommentBox(): React.ReactElement {
 
 
                     <button
-                            disabled = { downVote }
-                            onClick = { () => {
-                                                setDownVote( true )
-                                                setUpVote(false)
-                                                setCount( upVote ? count - 2 : count - 1 )
-                                                }    
-                                         }
+                            disabled = { downVote || count == 0 }
+                            onClick = { handleDownVote }
                     >
                         <img
                              src = { minusIcon } 
                              alt = "minus"
-                             style = {{ filter: !downVote ? 'brightness(0.7)' : 'brightness(1)', }}  
+                             style = {{ filter: !downVote ? 'brightness(0.7)' : 'brightness(1)', }}
+                             className = 'p-1'  
                         />
                         <h2>  </h2>
                     </button>
                 </div>
 
                 <div className = 'flex flex-col gap-2 h-32 w-full ' >
-
                     <div className = 'flex justify-between w-full  ' >
-                        <div className = 'flex justify-around items-center'>
+                        <div className = 'flex justify-around items-center gap-4'>
                             <img src = { avatar } alt = "" className = 'w-9 h-9' />
                             <h1> Hello world! </h1>
                         </div>
 
                         <button 
-                                className = 'h-6 flex items-center justify-around p-2 gap-2'
-                                onClick = { () => setReply(!reply) }
+                            className = 'h-6 flex items-center justify-around p-2 gap-2'
+                            onClick = { () => setReply(!reply) }
                         >
                             <img src = { replyIcon } alt = "" />
                             Reply
